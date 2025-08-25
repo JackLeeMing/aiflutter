75e82c0494c05e263536fcbb4afa3e07c85d9cd5
JiaKuiLee <JackLeeMing@outlook.com>
2025-08-25 15:42:11 +0800

    try to add new macos pro

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "cflutter.jaquelee.com.smart.aiflutter"
+        applicationId = "flutter.jaquelee.com.smart.aiflutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
